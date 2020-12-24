a7eb49d64c5816c9e96a24e95ba3f5de33b8d6a7
inoisy <inoisy@bk.ru>
2020-12-24 21:37:34 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,5 +63,17 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    babel: {
+      sourceType: 'unambiguous',
+      presets: [
+        ["@nuxt/babel-preset-app", {
+          corejs: {
+            version: 2
+          }
+        }],
+      ]
+    },
+    transpile: ["@nuxtjs/vuetify", /^aos/, /^@nuxtjs.*/, "nuxt-vuex-localstorage"],
+    extend(config, ctx) { }
   }
 }
